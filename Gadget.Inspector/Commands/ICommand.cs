c017f903b8e12f7bed30a25309e6f6ac9a800de6
charconstpointer <attwistingnether@gmail.com>
2020-09-27 23:15:15 +0200

    removed unused code

--- Gadget.Inspector/Commands/ICommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Gadget.Inspector.Commands
-{
-    public interface ICommand
-    {
-        public void Execute(ICollection<Service> services, string argument = "");
-    }
-}
