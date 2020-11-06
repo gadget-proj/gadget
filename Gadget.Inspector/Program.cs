a40104ea97f9233f581eeb14a6f592dbcd0c8e98
charconstpointer <attwistingnether@gmail.com>
2020-11-06 01:52:14 +0100

    added inspector dependencies

--- Gadget.Inspector/Program.cs
+++ Gadget.Inspector/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Channels;
+﻿using System;
+using System.Threading.Channels;
 using Gadget.Inspector.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,18 +20,9 @@ namespace Gadget.Inspector
                     services.AddLogging();
                     services.AddScoped(_ => Channel.CreateUnbounded<string>());
                     services.AddHostedService<ServicesWatcher>();
-                    services.AddHostedService<Inspector>()
+                    services.AddTransient(_ => new Uri("https://localhost:44347/gadget"));
+                    services.AddHostedService<Inspector>();
                     services.AddHostedService<Producer>();
                 });
-
-        private static async Task Main()
-        {
-            var l = new LoggerFactory();
-            var logger = l.CreateLogger<Inspector>();
-            var inspector = new Inspector(new Uri("https://localhost:44347/gadget"), logger);
-            await inspector.Start();
-            Console.WriteLine("Inspector started");
-            Console.ReadKey();
-        }
     }
 }
