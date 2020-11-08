e809e261fa0e843df1d6eaddab926962b558ffe1
Marcin Mojski <marcin_mojski@interia.pl>
2020-11-08 13:10:55 +0100

    tmp

--- Gadget.Inspector/Extensions/InspectorExtensions.cs
+++ Gadget.Inspector/Extensions/InspectorExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Channels;
 using Gadget.Messaging;
 using Gadget.Messaging.ServiceMessages;
@@ -13,7 +14,9 @@ namespace Gadget.Inspector.Extensions
             services.AddScoped(_ => Channel.CreateUnbounded<ServiceStatusChanged>());
             services.AddScoped(_ => Channel.CreateUnbounded<MachineHealthData>());
             services.AddTransient(_ => new Uri("https://localhost:5001/gadget"));
+            services.AddTransient(_ => new PerformanceCounter());
             services.AddHostedService<Services.Inspector>();
+            services.AddHostedService<MachineHealthWatcher>();
             return services;
         }
     }
