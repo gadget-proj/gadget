027b868b6791091aa010f56a55e9f6bbeffb6c2d
charconstpointer <attwistingnether@gmail.com>
2020-11-15 16:04:24 +0100

    simplify service monitoring, removed Watcher.cs and inter process communication via channels

--- Gadget.Inspector/Extensions/InspectorExtensions.cs
+++ Gadget.Inspector/Extensions/InspectorExtensions.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Threading.Channels;
-using Gadget.Inspector.Services;
 using Gadget.Inspector.Transport;
-using Gadget.Messaging;
-using Microsoft.AspNetCore.SignalR.Client;
 using Gadget.Messaging.Events;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Gadget.Inspector.Extensions
@@ -13,7 +11,7 @@ namespace Gadget.Inspector.Extensions
     {
         public static IServiceCollection AddInspector(this IServiceCollection services)
         {
-            services.AddScoped(provider =>
+            services.AddScoped(_ =>
             {
                 var hubConnection = new HubConnectionBuilder()
                     .WithAutomaticReconnect()
@@ -26,7 +24,6 @@ namespace Gadget.Inspector.Extensions
             services.AddScoped(_ => Channel.CreateUnbounded<ServiceStatusChanged>());
             services.AddTransient(_ => new Uri("https://localhost:5001/gadget"));
             services.AddHostedService<Services.Inspector>();
-            services.AddHostedService<Watcher>();
             return services;
         }
     }
--- Gadget.Inspector/Services/Inspector.cs
+++ Gadget.Inspector/Services/Inspector.cs
@@ -19,13 +19,16 @@ namespace Gadget.Inspector.Services
         private readonly Channel<ServiceStatusChanged> _channel;
         private readonly ILogger<Inspector> _logger;
         private readonly IControlPlane _controlPlane;
+        private readonly IDictionary<string, ServiceControllerStatus> _statuses;
 
         public Inspector(Channel<ServiceStatusChanged> channel, IControlPlane controlPlane, ILogger<Inspector> logger)
         {
+            _statuses = new Dictionary<string, ServiceControllerStatus>();
             _channel = channel;
             _controlPlane = controlPlane;
             _logger = logger;
         }
+
         private void RegisterHandlers()
         {
             _controlPlane.RegisterHandler<StopService>("StopService", command =>
@@ -45,36 +48,57 @@ namespace Gadget.Inspector.Services
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             await RegisterAgent();
+            _logger.LogInformation($"Starting Watcher ${DateTime.UtcNow}");
             while (!stoppingToken.IsCancellationRequested)
             {
-                try
+                foreach (var serviceController in ServiceController.GetServices())
                 {
-                    await _channel.Reader.WaitToReadAsync(stoppingToken);
-                    var @event = await _channel.Reader.ReadAsync(stoppingToken);
-                    _logger.LogInformation($"Service {@event.Name} status has changed to {@event.Status}");
-                    await _controlPlane.Invoke("ServiceStatusChanged", @event);
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e.Message);
-                }
-            }
-
-            async Task RegisterAgent()
-            {
-                RegisterHandlers();
-
-                var registerNewAgent = new RegisterNewAgent
-                {
-                    Agent = Environment.MachineName.Replace("-",""),
-                    Services = ServiceController.GetServices().Select(s => new Service
+                    if (!_statuses.ContainsKey(serviceController.ServiceName))
                     {
-                        Name = s.ServiceName,
-                        Status = s.Status.ToString()
-                    })
-                };
-                await _controlPlane.Invoke("Register", registerNewAgent);
+                        _statuses[serviceController.ServiceName] = serviceController.Status;
+                        await UpdateStatus(serviceController, stoppingToken);
+                    }
+
+                    var previousStatus = _statuses[serviceController.ServiceName];
+                    var currentStatus = serviceController.Status;
+                    if (currentStatus == previousStatus)
+                    {
+                        continue;
+                    }
+
+                    await UpdateStatus(serviceController, stoppingToken);
+                    _statuses[serviceController.ServiceName] = currentStatus;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
             }
+        }
+
+        private async Task RegisterAgent()
+        {
+            RegisterHandlers();
+
+            var registerNewAgent = new RegisterNewAgent
+            {
+                Agent = Environment.MachineName.Replace("-", ""),
+                Services = ServiceController.GetServices().Select(s => new Service
+                {
+                    Name = s.ServiceName,
+                    Status = s.Status.ToString()
+                })
+            };
+            await _controlPlane.Invoke("Register", registerNewAgent);
+        }
+
+        private async Task UpdateStatus(ServiceController serviceController, CancellationToken stoppingToken)
+        {
+            var @event = new ServiceStatusChanged
+            {
+                Name = serviceController.ServiceName,
+                Status = serviceController.Status.ToString(),
+                AgentId = Environment.MachineName.Replace("-", "")
+            };
+            await _controlPlane.Invoke("ServiceStatusChanged", @event);
         }
     }
 }
