c89709f275376a79c0b8554c44d585d4136f1cbb
charconstpointer <attwistingnether@gmail.com>
2020-12-24 14:45:51 +0100

    ServiceStatusChangedConsumer.cs hub

--- Gadget.Server/Agents/Consumers/ServiceStatusChangedConsumer.cs
+++ Gadget.Server/Agents/Consumers/ServiceStatusChangedConsumer.cs
@@ -1,21 +1,26 @@
 using System.Threading.Tasks;
 using Gadget.Messaging.Events;
+using Gadget.Server.Hubs;
 using MassTransit;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 
 namespace Gadget.Server.Agents.Consumers
 {
     public class ServiceStatusChangedConsumer : IConsumer<IServiceStatusChanged>
     {
+        private readonly IHubContext<GadgetHub> _hub;
         private readonly ILogger<ServiceStatusChangedConsumer> _logger;
 
-        public ServiceStatusChangedConsumer(ILogger<ServiceStatusChangedConsumer> logger)
+        public ServiceStatusChangedConsumer(ILogger<ServiceStatusChangedConsumer> logger, IHubContext<GadgetHub> hub)
         {
             _logger = logger;
+            _hub = hub;
         }
 
         public async Task Consume(ConsumeContext<IServiceStatusChanged> context)
         {
+            await _hub.Clients.Group("dashboard").SendAsync("ServiceStatusChanged", context.Message);
             _logger.LogInformation($"{context.Message.GetType()} received");
         }
     }
