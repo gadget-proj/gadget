d0facff00ded74712b3087a6f05a5fff30fbf65e
charconstpointer <attwistingnether@gmail.com>
2020-12-25 14:12:49 +0100

    Inspector.cs background service watching for changes in services

--- Gadget.Inspector/Inspector.cs
+++ Gadget.Inspector/Inspector.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Threading;
 using System.Threading.Tasks;
-using Gadget.Messaging;
 using Gadget.Messaging.Commands;
+using Gadget.Messaging.Events;
 using MassTransit;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -15,6 +16,10 @@ namespace Gadget.Inspector
     {
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly ILogger<Inspector> _logger;
+
+        private readonly IDictionary<string, ServiceControllerStatus> _services =
+            new Dictionary<string, ServiceControllerStatus>();
+
         public Inspector(IPublishEndpoint publishEndpoint, ILogger<Inspector> logger)
         {
             _publishEndpoint = publishEndpoint;
@@ -23,7 +28,6 @@ namespace Gadget.Inspector
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("before");
             await _publishEndpoint.Publish<IRegisterNewAgent>(new
             {
                 Agent = Environment.MachineName,
@@ -34,7 +38,34 @@ namespace Gadget.Inspector
                 // })
                 Services = Enumerable.Empty<object>()
             }, stoppingToken);
-            _logger.LogInformation("after");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                foreach (var serviceController in ServiceController.GetServices())
+                {
+                    serviceController.Refresh();
+                    var current = serviceController.Status;
+                    if (!_services.TryGetValue(serviceController.ServiceName, out var previous))
+                    {
+                        _services[serviceController.ServiceName] = current;
+                        continue;
+                    }
+
+                    if (current == previous)
+                    {
+                        continue;
+                    }
+                    _services[serviceController.ServiceName] = current;
+                    await _publishEndpoint.Publish<IServiceStatusChanged>(new
+                    {
+                        Agent = Environment.MachineName,
+                        ServicName = serviceController.ServiceName,
+                        Status = current.ToString()
+                    }, stoppingToken);
+
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+            }
         }
     }
 }
--- Gadget.Messaging/Events/IServiceStatusChanged.cs
+++ Gadget.Messaging/Events/IServiceStatusChanged.cs
@@ -2,6 +2,7 @@ namespace Gadget.Messaging.Events
 {
     public interface IServiceStatusChanged
     {
+        string  Agent { get; }
         string ServiceName { get; }
         string Status { get; }
     }
