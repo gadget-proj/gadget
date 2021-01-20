c066f9adcebc7651e1b69f1f9674eb18f39534f3
charconstpointer <attwistingnether@gmail.com>
2021-01-20 17:00:08 +0100

    ServiceEvent.cs id as guid, ef core properties

---
+++ Gadget.Server/Agents/AgentsService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gadget.Messaging.Contracts.Commands;
+using Gadget.Server.Agents.Dto;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Gadget.Server.Agents
+{
+    public class AgentsService : IAgentsService
+    {
+        private readonly GadgetContext _context;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<AgentsService> _logger;
+
+        public AgentsService(GadgetContext context, IPublishEndpoint publishEndpoint, ILogger<AgentsService> logger)
+        {
+            _context = context;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<AgentDto>> GetAgents()
+        {
+            var agents = await _context.Agents.ToListAsync();
+            return await Task.FromResult(agents.Select(a => new AgentDto
+            {
+                Name = a.Name,
+                Address = a.Address
+            }));
+        }
+
+        public async Task<IEnumerable<ServiceDto>> GetServices(string agentName)
+        {
+            var machine = _context.Agents
+                .Include(a => a.Services)
+                .ThenInclude(s => s.Events.Take(20))
+                .FirstOrDefault(x => x.Name == agentName);
+            var services = machine?.Services;
+            var dto = services?.Select(s => new ServiceDto(s.Name, s.Status, s.LogOnAs, s.Description, s.Events));
+            return await Task.FromResult(dto);
+        }
+
+        public async Task StartService(string agentName, string serviceName)
+        {
+            try
+            {
+                await _publishEndpoint.Publish<IStartService>(new
+                    {
+                        ServiceName = serviceName,
+                        Agent = agentName
+                    },
+                    context => { context.SetRoutingKey(serviceName); });
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e.Message);
+            }
+        }
+
+        public async Task StopService(string agentName, string serviceName)
+        {
+            try
+            {
+                await _publishEndpoint.Publish<IStopService>(new
+                    {
+                        ServiceName = serviceName,
+                        Agent = agentName
+                    },
+                    context => { context.SetRoutingKey(serviceName); });
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e.Message);
+            }
+        }
+    }
+}
--- Gadget.Server/GadgetContext.cs
+++ Gadget.Server/GadgetContext.cs
@@ -13,15 +13,20 @@ namespace Gadget.Server
         {
             modelBuilder.Entity<Agent>(builder => builder.HasKey(a => a.Id));
             modelBuilder.Entity<Agent>(builder => builder.HasMany(a => a.Services));
+            modelBuilder.Entity<Agent>(builder => builder.Property(a => a.Name));
+            modelBuilder.Entity<Agent>(builder => builder.Property(a => a.Address));
+            
             modelBuilder.Entity<Service>(builder => builder.HasKey(a => a.Id));
             modelBuilder.Entity<Service>(builder => builder.Property(a => a.Name));
             modelBuilder.Entity<Service>(builder => builder.Property(a => a.Status));
             modelBuilder.Entity<Service>(builder => builder.Property(a => a.LogOnAs));
             modelBuilder.Entity<Service>(builder => builder.Property(a => a.Description));
             modelBuilder.Entity<Service>(builder => builder.HasMany(s => s.Events));
-            modelBuilder.Entity<Agent>(builder => builder.Property(a => a.Name));
-            modelBuilder.Entity<Agent>(builder => builder.Property(a => a.Address));
+            
             modelBuilder.Entity<ServiceEvent>(builder => builder.HasKey(s => s.Id));
+            modelBuilder.Entity<ServiceEvent>(builder => builder.Property(s => s.Id));
+            modelBuilder.Entity<ServiceEvent>(builder => builder.Property(s => s.Status));
+            modelBuilder.Entity<ServiceEvent>(builder => builder.Property(s => s.CreatedAt));
         }
 
         public DbSet<Agent> Agents { get; set; }
