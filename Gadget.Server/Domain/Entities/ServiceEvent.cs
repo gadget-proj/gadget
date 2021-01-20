128901fb6022f397e3a3bcee07925c4e94f6f4b7
Marcin Mojski <marcin_mojski@interia.pl>
2021-01-20 17:18:00 +0100

    tmp

--- Gadget.Server/Domain/Entities/ServiceEvent.cs
+++ Gadget.Server/Domain/Entities/ServiceEvent.cs
@@ -7,12 +7,13 @@ namespace Gadget.Server.Domain.Entities
         public int Id { get; private set; }
         public string Status { get; private set; }
         public DateTime CreatedAt { get; private set; }
+        public Guid ServiceId { get; set; }
 
         private ServiceEvent()
         {
         }
 
-        public ServiceEvent(string status)
+        public ServiceEvent(string status, Guid serviceId)
         {
             Status = status ?? throw new ArgumentNullException(nameof(status));
             CreatedAt = DateTime.UtcNow;
