e7f6043ea842b1d6eab01bc18bc98455bad70d16
charconstpointer <attwistingnether@gmail.com>
2021-04-13 22:25:13 +0200

    init

---
+++ Gadget.Auth/Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gadget.Auth.Helpers;
+using Gadget.Auth.Persistence;
+using Gadget.Auth.Providers;
+using Gadget.Auth.Services;
+using Gadget.Auth.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Gadget.Auth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<TokenManager>();
+            services.AddControllers();
+            services.AddDbContext<AuthContext>(builder => builder.UseSqlite("Data Source=auth.db"));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Gadget.Auth", Version = "v1"});
+            });
+            services.AddTransient<ILoginProvider, PrMockProvider>();
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddTransient<AuthorizationHelper>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    corsBuilder =>
+                    {
+                        corsBuilder
+                            .WithOrigins("localhost:3000")
+                            .WithOrigins("http://localhost:3000")
+                            .WithOrigins("localhost:5000")
+                            .WithOrigins("http://localhost:5000")
+                            .WithOrigins("https://localhost:5005")
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gadget.Auth v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
